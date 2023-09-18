ecf15e2cd3f8be77f81436866f839b4d2e923d79
rodrigoreeis <br.rodrigoreis@gmail.com>
2023-09-18 06:52:14 -0300

    feat: create db connection

---
+++ database/db.go
@@ -0,0 +1,23 @@
+package database
+
+import (
+	"log"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var (
+	DB  *gorm.DB
+	err error
+)
+
+func Connection() {
+	dsn := "host=localhost user=root password=root dbname=root port=5432 sslmode=disable"
+	DB, err = gorm.Open(postgres.Open(dsn))
+
+	if err != nil {
+		log.Panic("error connection database", err)
+	}
+
+}
--- main.go
+++ main.go
@@ -1,7 +1,11 @@
 package main
 
-import "github.com/rodrigoreeis/api-go-rest/routes"
+import (
+	"github.com/rodrigoreeis/api-go-rest/database"
+	"github.com/rodrigoreeis/api-go-rest/routes"
+)
 
 func main() {
+	database.Connection()
 	routes.HandleRequest()
 }
