10ffaa82c2a31f5195f3393e0333bcf42915dd18
rodrigoreeis <br.rodrigoreis@gmail.com>
2023-09-18 06:52:45 -0300

    feat: create controllers to get personalities

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -1,10 +1,32 @@
 package controllers
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/rodrigoreeis/api-go-rest/database"
+	"github.com/rodrigoreeis/api-go-rest/models"
 )
 
 func Home(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "tests")
+	fmt.Fprint(w, "Home page")
+}
+
+func Heath(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+}
+
+func GetPersonalities(w http.ResponseWriter, r *http.Request) {
+	var p []models.Personalities
+	database.DB.Find(&p)
+	json.NewEncoder(w).Encode(p)
+}
+
+func GetPersonalityById(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	var personality models.Personalities
+	database.DB.First(&personality, vars["id"])
+	json.NewEncoder(w).Encode(personality)
 }
