ea77cfd9fa34e9693b4568cc350115e75335736f
rodrigoreeis <br.rodrigoreis@gmail.com>
2023-09-18 06:52:54 -0300

    feat: crate handle routes

--- routes/routes.go
+++ routes/routes.go
@@ -5,11 +5,16 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/gorilla/mux"
 	"github.com/rodrigoreeis/api-go-rest/controllers"
 )
 
 func HandleRequest() {
-	http.HandleFunc("/", controllers.Home)
+	r := mux.NewRouter()
+	r.HandleFunc("/", controllers.Home)
+	r.HandleFunc("/heath", controllers.Heath)
+	r.HandleFunc("/api/v1/personalities/", controllers.GetPersonalities).Methods("GET")
+	r.HandleFunc("/api/v1/personalities/{id}", controllers.GetPersonalityById).Methods("GET")
 	fmt.Println("server localhost port:8000")
-	log.Fatal(http.ListenAndServe(":8000", nil))
+	log.Fatal(http.ListenAndServe(":8000", r))
 }
